cd0069310e28ec8d7b6a4fe82b975b7222fdbddb
Maharshpatel1709 <54908868+Maharshpatel1709@users.noreply.github.com>
2020-10-01 02:31:20 +0530

    Create chapter10.py
    Created a live video facial detector.

---
+++ chapter10.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow('frame', frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
